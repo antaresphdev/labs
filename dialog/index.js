59e37bcf88b3c7221d0d68e76e06bece2f3cdde5
Francis Rubio <francis.rubio@dice205.com>
2023-05-27 15:50:29 +0800

    Add example for dialog

---
+++ dialog/index.js
@@ -0,0 +1,11 @@
+const dialogTriggers = document.querySelectorAll('button[data-dialog]')
+dialogTriggers.forEach(button => {
+  const dialogId = button.dataset.dialog
+
+  if (dialogId != null && dialogId.length > 0) {
+    const dialog = document.getElementById(dialogId)
+    if (dialog != null) {
+      button.addEventListener('click', e => dialog.showModal())
+    }
+  }
+})
